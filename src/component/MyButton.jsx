0b4c22b22d2cf4b5017f6dc34f3f7884872c9ebe
Aekthana <okkratoo@hotmail.com>
2023-03-25 00:07:27 +0700

    'start-pj'

---
+++ src/component/MyButton.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+const MyButton = ()=>{
+    return(
+        <>
+        <br />
+        <Button color='secondary'>Back</Button>
+        <Button color='secondary' type="submit">Next</Button>
+        </>
+    )
+}
+export default MyButton
